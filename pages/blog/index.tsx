42bf580f28021a26a8271bc777cbbf26aee9dfe9
garry-singh <garry.singh@live.com.au>
2022-07-30 21:50:41 +1000

    Get blog post data using getStaticProps

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,9 +1,52 @@
-const Blog = () => {
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { GetStaticProps } from "next";
+
+type Props = {
+  posts: {
+    slug: string;
+    frontmatter: {
+      [key: string]: any;
+    };
+  }[];
+};
+
+const Blog = ({ posts }: Props) => {
   return (
     <div>
-      <h1>THIS IS A BLOG</h1>
+      {posts.map((post) => (
+        <p key={post.frontmatter.title}>{post.frontmatter.title}</p>
+      ))}
     </div>
   );
 };
 
 export default Blog;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  // Get files from posts directory
+  const files = fs.readdirSync(path.join("posts"));
+
+  // Get slug and front matter from posts
+  const posts = files.map((file) => {
+    // Create slug
+    const slug = file.replace(".md", "");
+
+    // Get front matter
+    const markdownWithMeta = fs.readFileSync(path.join("posts", file), "utf-8");
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
---
+++ posts/test.md
@@ -0,0 +1,12 @@
+---
+title: "Test Post"
+date: "August 24th 2022"
+excerpt: "This is some text"
+---
+
+## This is a Heading Two
+
+- Item 1
+- Item 2
+
+Some test paragraph
